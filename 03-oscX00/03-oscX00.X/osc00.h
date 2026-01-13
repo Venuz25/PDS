c9720bc4f477f171ffedd85a2c5c5696fff5da78
Areli Guevara <115609722+Venuz25@users.noreply.github.com>
2026-01-13 17:34:56 -0600

    Add files via upload

---
+++ 03-oscX00/03-oscX00.X/osc00.h
@@ -0,0 +1,16 @@
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+//file name oscx00.h
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include <libpic30.h>
+
+#define _XTAL_FREQ 4000000
+
+
